8f1a413ec2fcb1b1fd7d2b600268008aa709f2c7
icarillius <icarillius@gmail.com>
2025-10-07 11:56:23 +0100

    UP Rec

--- Source/CPPTestBed/Private/MyReciever.cpp
+++ Source/CPPTestBed/Private/MyReciever.cpp
@@ -1,16 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MyReciever.h"
-//#include "EngineUtils.h"
 
 // Sets default values
 AMyReciever::AMyReciever()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	
-	AMyDispatcher myDispatcher;
-	
+
 }
 
 void AMyReciever::RunBroadCast()
@@ -22,12 +19,11 @@ void AMyReciever::RunBroadCast()
 void AMyReciever::BindtoActorDelegate(AMyDispatcher* MyDispatcter)
 {
 	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, FString::Printf(TEXT("Message In")));
-	
-	
 
 	if (MyDispatcter)
 	{
 		MyDispatcter->TestDelegate.AddDynamic(this, &AMyReciever::RunBroadCast);
+		RunBroadCast();
 	}
 }
 
@@ -37,9 +33,8 @@ void AMyReciever::BeginPlay()
 	Super::BeginPlay();
 
 	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::Printf(TEXT("Reciever Exists")));
-	
-	AMyDispatcher myDispatcher;
 
+	
 }
 
 // Called every frame
