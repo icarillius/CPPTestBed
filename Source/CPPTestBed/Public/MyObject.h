5a382ec2634668b9ff2b83745b19fac3c41c6647
Ian Griffiths <icarillius@gmail.com>
2025-10-14 10:02:09 +0100

    UObjects

---
+++ Source/CPPTestBed/Public/MyObject.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "MyObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class CPPTESTBED_API UMyObject : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite)
+	int MyObjectValue = 10;
+	
+
+};
---
+++ enc_temp_folder/d8c7acd5871e5845953b46fa4d5d218d/MyObject.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "MyObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class CPPTESTBED_API UMyObject : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	int MyObjectValue = 10;
+	
+};
